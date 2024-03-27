30d361567a241606137dbec91a16c437a0ca4da2
sinh <sinh@example.com>
2024-03-27 11:42:32 +0700

    tao model menu and booking

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    Title = models.CharField(max_length =255)
+    Price = models.DecimalField() 
+    Inventory = models.IntegerField()
+
+class Booking(models.Model):
+    Name = models.CharField(max_length =255)
+    No_of_guests = models.IntegerField() 
+    BookingDate = models.DateTimeField()
+    
